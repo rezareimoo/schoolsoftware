c88b5443e3b50b7c466260046453e582f7bf41aa
Reza Reimoo <39816216+rezareimoo@users.noreply.github.com>
2020-08-06 21:16:41 -0400

    added files

---
+++ Person.py
@@ -0,0 +1,21 @@
+class Person:
+	def __init__(self, first_name, last_name, age, address, gender, phone):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.age = age
+		self.address = address
+		self.gender = gender
+		self.phone = phone
+		self.phone = phone
+
+	def get_first_name(self):
+		return self.first_name
+
+	def get_last_name(self):
+		return self.last_name
+
+	def get_age(self):
+		return self.age
+
+	def get_address(self):
+		self.address
