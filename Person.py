cd17e9f0ce9bf42c5e595a49e70eba11e6e41582
Reza Reimoo <39816216+rezareimoo@users.noreply.github.com>
2020-08-06 21:46:42 -0400

    person and student stuff

--- Person.py
+++ Person.py
@@ -1,11 +1,27 @@
 from Address import Address
 class Person:
-	def __init__(self, first_name, last_name, date_of_birth, gender, phone):
+	def __init__(self, first_name, last_name, date_of_birth, gender, phone, address):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.date_of_birth = date_of_birth
 		self.gender = gender
 		self.phone = phone
+		self.addresses = []
+
+		if isinstance (address, Address):
+			self.addresses.append(address)
+		elif isinstance(address, list):
+			for entry in address:
+				if not isinstance(entry, Address):
+					raise Error("Invaild Address...")
+			self.addresses = address
+		else:
+			raise Error("Invaild Address...")
+	def add_address(self, address):
+		if not isinstance(address, Address):
+			raise Error("Invaild Address...")
+			
+		self.addresses.append(address)
 
 	def get_first_name(self):
 		return self.first_name
